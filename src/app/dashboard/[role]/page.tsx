c59051bc51bc33f5903ce2eff3e02d91e7e3d32b
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2025-05-07 11:05:05 +0700

    add v1 version

---
+++ src/app/dashboard/[role]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const DashBoardPage = ({ params }: { params: { role: string } }) => {
+  console.log(params)
+  return <div>DashBoardPage</div>
+}
+
+export default DashBoardPage
